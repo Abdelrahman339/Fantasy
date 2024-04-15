7defce16c903aded1d6bb51b522e6d1c8be30218
Abdelrahman339 <abdelrahmanamr.eltawab@gmail.com>
2024-04-15 06:24:06 +0200

    added the code in files

--- Competition/Game/Game.h
+++ Competition/Game/Game.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+class Game
+{
+};
+class footballPlayer
+{
+    string name;
+    float rating;
+    int goals;
+    int assists;
+    bool redCards;
+    bool yellowCards;
+};
--- Data/Teams/Teams.h
+++ Data/Teams/Teams.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+class Teams
+{
+    int points;
+};
+
+class Footballer
+{
+    string name;
+    int age;
+    string position;
+    float price;
+    bool captin;
+    int goals;      // per season
+    int assists;    // per season
+    int redCard;    // per season
+    int yellowCard; // per season
+};
