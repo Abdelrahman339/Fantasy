54853c6444590e595682e563ecefac43e44d0a55
abdullah12q <abdullahelsiefy12@gmail.com>
2024-04-22 17:18:41 +0200

    admin

--- Admin/Admin.h
+++ Admin/Admin.h
@@ -1,6 +1,39 @@
 #pragma once
 #include <iostream>
+#include <vector>
+#include "list"
+#include "queue"
+#include "User.h"
+#include "unordered_map"
+#include "Teams.h"
+#include"Leagues.h"
 using namespace std;
 class Admin
 {
+protected:
+
+
+
+
+public:
+	void AdminMenu(unordered_map<string, User>& Users, unordered_map<string, Teams>& Team, unordered_map<string, Leagues>& League);
+
+	//users
+	void AboutUsers(unordered_map<string, User>& Users, unordered_map<string, Teams>& Team, unordered_map<string, Leagues>& League);
+	void DisplayAllUsers(unordered_map<string, User>& Users);
+	void ShowAndEditUser(unordered_map<string, User>& Users);
+	void UserSquadAndPlayers(unordered_map<string, User>& Users);
+	void DeleteUser(unordered_map<string, User>& Users);
+
+	//teams
+	void AboutTeams(unordered_map<string, User>& Users, unordered_map<string, Teams>& Team, unordered_map<string, Leagues>& League);
+	void DisplayAllTeams(unordered_map<string, Teams>& Team);
+	void ShowAndEditTeam(unordered_map<string, Teams>& Team);
+
+	//leagues
+	void AboutLeagues(unordered_map<string, User>& Users, unordered_map<string, Teams>& Team, unordered_map<string, Leagues>& League);
+	void DisplayAllLeagues(unordered_map<string, Leagues>& League);
+	void ShowAndEditLeague(unordered_map<string, Leagues>& League);
+
+	void PauseAndClear(string GoTo);
 };
