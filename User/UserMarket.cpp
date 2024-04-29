c537e3550433d1f548589bae11d8e4da235e5a58
Abdelrahman339 <abdelrahmanamr.eltawab@gmail.com>
2024-04-29 10:17:43 +0300

    Moved showTopPlayer from play .cpp to market.cpp

--- User/UserMarket.cpp
+++ User/UserMarket.cpp
@@ -35,7 +35,48 @@ invalid:
 	}
 };
 
+void User::showtopPlayer(unordered_map<string, Footballer> TopPlayer, User& currentUser, unordered_map<string, User>& Users, Teams& team)
+{
+	string footballerName;
+	int choice;
+invalid:
+	cout << "Welcome to top player in the session!" << endl;
+	User::Format343(TopPlayer);
+	system("cls");
 
+	cout << "1-Show information about player\n2-buy player\n3-Replace player\n4-Go back." << endl;
+	cin >> choice;
+	if (choice == 1) {
+		cout << "Enter the name of the player you want to show information about" << endl;
+		cin >> footballerName;
+		if (TopPlayer.count(footballerName) > 0)
+		{
+			showPlayerInfo(TopPlayer.at(footballerName));
+		}
+		else
+		{
+			cout << "Please enter a valid name" << endl;
+			goto invalid;
+		}
+	}
+	else if (choice == 2)
+	{
+		buy(currentUser, team, TopPlayer, Users);
+
+	}
+	else if (choice == 3) {
+		replace(currentUser, team, TopPlayer, Users);
+	}
+	else if (choice == 4) {
+		Market(currentUser, team, TopPlayer, Users);
+	}
+	else {
+		cout << "Invalid choice.Please select valid choice" << endl;
+		goto invalid;
+	}
+
+
+}
 
 
 //void User::search(string footballerName, Teams team, User currentUser, string status, vector<League>  leagues)
