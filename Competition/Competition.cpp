cff8bcbd7dd5a621adb283123b8852c3415239aa
Omar Hassan <you@example.com>
2024-05-03 21:33:07 +0200

    UpdateAllpoints foundation

--- Competition/Competition.cpp
+++ Competition/Competition.cpp
@@ -1,337 +1,361 @@
-//#include <iostream>
-//#include <queue>
-//#include<list>
-//#include "Competition.h"
-//#include "Teams.h"
-//#include"Game/Game.h"
-//#include <unordered_map>
-//#include "User.h"
-//#include <regex>
-//
-//
-//using namespace std;
-//
-//
-//
-//
-//bool Competition::IsManOfTheMatch(string currentMOTM, string playerName)
-//{
-//	return (currentMOTM == playerName);
-//}
-//
-//
-//bool Competition::checkPosition(string footballerPosition) {
-//
-//	if (footballerPosition == "defender" || footballerPosition == "goalkeeper") {
-//		return true;
-//	}
-//
-//	else
-//		return false;
-//
-//}
-//
-//char Competition::priceCalculation(int points) {
-//
-//
-//
-//	if (points < 3) {
-//		return '0';
-//	}
-//	else if (points < 6) {
-//		return '1';
-//	}
-//	else if (points < 9) {
-//		return '2';
-//	}
-//	else if (points < 12) {
-//		return '3';
-//	}
-//	else if (points < 15) {
-//		return '4';
-//	}
-//	else {
-//		return '5';
-//	}
-//
-//}
-//
-//void Competition::removeCurrentGame(queue<Game> UserGames, list<Game>& allGames)
-//{
-//	while (!UserGames.empty())
-//	{
-//		auto gameit = allGames.begin();
-//		while (gameit != allGames.end())
-//		{
-//			if ((*gameit).getGameId() == UserGames.front().getGameId()) {
-//				break;
-//			}
-//		}
-//		allGames.erase(gameit);
-//
-//
-//
-//
-//		UserGames.pop();
-//	}
-//}
-//
-//void Competition::ReducePoints(string footballerName, User& currentUser, string violence, string status, Teams& team)
-//{
-//	if (status == "User") {
-//		if (violence == "RedCard")
-//		{
-//			currentUser.GetMainSquad().at(footballerName).AddTotalpoints(-2);
-//			currentUser.AddPoints(-2);
-//			currentUser.addBalance(-20);
-//		}
-//		if (violence == "YellowCard") {
-//			currentUser.GetMainSquad().at(footballerName).AddTotalpoints(-1);
-//			currentUser.AddPoints(-1);
-//			currentUser.addBalance(-5);
-//		}
-//	}
-//	else if (status == "Footballer") {
-//		if (violence == "RedCard")
-//		{
-//			team.getFootballPlayer().at(footballerName).AddTotalpoints(-2);
-//		}
-//		if (violence == "YellowCard") {
-//			team.getFootballPlayer().at(footballerName).AddTotalpoints(-1);
-//		}
-//	}
-//
-//
-//
-//}
-//
-//void Competition::updatePoints(string footballerName, User& currentUser, string contributes, string status, Teams& team, string footballerPosition)
-//{
-//	//"goals 5 & assists 6"
-//	regex pattern(R"(&)");
-//	int numberofGoals;
-//	if (regex_match(contributes, pattern)) { // if the player scord and did assist
-//
-//		addGoalsAssistPoints(contributes, currentUser, footballerName, status, team);
-//	}
-//
-//	else if (!regex_match(contributes, pattern))
-//	{
-//		regex Goalpattern(R"(Goal)");
-//		regex Assistpattern(R"(Assist)");
-//		if (regex_match(contributes, Goalpattern)) {
-//
-//			addPoints(contributes, currentUser, footballerName, Competition::goalPoints, status, team);
-//		}
-//		else if (regex_match(contributes, Assistpattern)) {
-//
-//			addPoints(contributes, currentUser, footballerName, Competition::assistPoints, status, team);
-//		}
-//		else {
-//			if (checkPosition(footballerPosition)) {
-//				currentUser.GetMainSquad().at(footballerName).AddTotalpoints(cleanSheetPoints);
-//				currentUser.AddPoints(cleanSheetPoints);
-//				currentUser.addBalance(10);
-//			}
-//		}
-//	}
-//	else
-//	{
-//		currentUser.GetMainSquad().at(footballerName).AddTotalpoints(-10);
-//		currentUser.AddPoints(-10);
-//	}
-//
-//}
-//
-//
-//void Competition::addPoints(string contributes, User currentUser, string footballerName, int numPerpoints, string status, Teams& team)
-//{
-//	int numberOfcontributes;
-//	regex pattern(R"(\d+)");
-//
-//	smatch match;
-//
-//	if (regex_search(contributes, match, pattern)) {
-//		numberOfcontributes = stoi(match[0]);
-//
-//	}
-//	if (status == "footballer") {
-//		team.getFootballPlayer().at(footballerName).AddTotalpoints(numPerpoints * numberOfcontributes);
-//	}
-//	else {
-//		currentUser.GetMainSquad().at(footballerName).AddTotalpoints(numPerpoints * numberOfcontributes);
-//		currentUser.AddPoints(numPerpoints * numberOfcontributes);
-//		currentUser.addBalance(numPerpoints * numberOfcontributes);
-//	}
-//}
-//
-//
-//
-//void Competition::addGoalsAssistPoints(string contributes, User currentUser, string footballerName, string status, Teams& team)
-//{
-//	regex pattern(R"(\d+)");
-//
-//	smatch matches;
-//	int goalsnum=0, assistsnum=0;
-//
-//	int foundNumbers = 0;
-//
-//	while (regex_search(contributes, matches, pattern)) {
-//		if (foundNumbers == 0) {
-//			goalsnum = stoi(matches[0]);
-//		}
-//		else {
-//			assistsnum = stoi(matches[0]);
-//		}
-//		contributes = matches.suffix().str();
-//		foundNumbers++;
-//	}
-//	int totalPoints = (goalsnum * Competition::goalPoints) + (assistsnum * Competition::assistPoints);
-//	if (status == "footballer") {
-//		team.getFootballPlayer().at(footballerName).AddTotalpoints(totalPoints);
-//	}
-//	else {
-//		currentUser.GetMainSquad().at(footballerName).AddTotalpoints(totalPoints);
-//		currentUser.AddPoints(totalPoints);
-//		currentUser.addBalance(totalPoints * 3);
-//	}
-//}
-//
-//void Competition::findPlayers(queue<Game>& UserGames, User& currentUser, string status, Teams& team)
-//{
-//
-//	Game currentGame = UserGames.front();
-//	while (!currentGame.getHighlightsOfTheMatch().empty())
-//	{
-//
-//		string currentPlayerinMatch = currentGame.getHighlightsOfTheMatch().top().getName();
-//		for (auto& kv : currentUser.GetMainSquad()) {
-//			string footballerName = kv.first;
-//			if (footballerName == currentPlayerinMatch)
-//			{
-//				updatePoints(currentPlayerinMatch, currentUser, currentGame.getHighlightsOfTheMatch().top().getContributions(), "User", team);
-//				ReducePoints(currentPlayerinMatch, currentUser, currentGame.getHighlightsOfTheMatch().top().getViolation(), "User", team);
-//
-//			}
-//		}
-//
-//		//go to the next player
-//		currentGame.getHighlightsOfTheMatch().pop();
-//	}
-//
-//
-//	//The end of the match
-//	UserGames.pop();
-//}
-//
-//void Competition::showAllGameHighlights(queue<Game>Usergames, list <Game>& allGame)
-//{
-//	char ans;
-//	removeCurrentGame(Usergames, allGame);
-//	cout << "Highlights of the week" << endl;
-//	for (Game game : allGame) {
-//		cout << game.getAwayTeam().getName() << User::spacing(10, ' ') << game.getHomeTeam().getName() << endl;
-//		cout << game.getScore() << endl;
-//	}
-//}
-//
-//void Competition::UpdateFootballerPrice(Footballer& player) // for all the players
-//{
-//	char tier = Competition::priceCalculation(player.GetTotalpoints());
-//	float currentPlayerPrice = player.GetPrice();
-//	float priceChange;
-//
-//	switch (tier) {
-//
-//	case '1':
-//		priceChange = currentPlayerPrice + 0.5f;
-//		break;
-//
-//	case '2':
-//		priceChange = currentPlayerPrice + 1.0f;
-//		break;
-//
-//	case '3':
-//		priceChange = currentPlayerPrice + 1.5f;
-//		break;
-//
-//	case '4':
-//		priceChange = currentPlayerPrice + 2.0f;
-//		break;
-//
-//	case '5':
-//		priceChange = currentPlayerPrice + 2.5f;
-//		break;
-//
-//	default:
-//		//if the player doesn't perfom with any of the tiers
-//		priceChange = currentPlayerPrice - 0.2f;
-//	}
-//
-//	player.SetPrice(priceChange);
-//	player.SetTotalpoints(0);
-//
-//
-//}
-//
-//void Competition::searchTeamInMatch(unordered_map<string, Footballer> TeamType, Game game) {
-//
-//
-//	Teams team;
-//	User currentUser;
-//	string status = "footballer";
-//	HighlightsOfTheMatch highlights = game.getHighlightsOfTheMatch().top();
-//
-//
-//	string currentMOTM = game.getManOfTheMatch();
-//
-//	for (auto& kv : TeamType) {
-//
-//		string playerName = kv.first;
-//		Footballer& currentFootballer = kv.second;
-//
-//		int ManOfTheMatchPoints = 0;
-//		string contributions = highlights.getContributions();
-//		string violation = highlights.getViolation();
-//
-//
-//
-//		if (Competition::IsManOfTheMatch(currentMOTM, currentFootballer.GetName()) == true) {
-//			ManOfTheMatchPoints = Competition::MOTM_Bonus;
-//		}
-//
-//		Competition::updatePoints(playerName, currentUser, contributions, status, team,currentFootballer.GetPosition());
-//		Competition::ReducePoints(playerName, currentUser, violation, status, team); // for deducing redCards and yellowcards points
-//
-//		int currentPoints = currentFootballer.GetTotalpoints();
-//		currentFootballer.AddTotalpoints(currentPoints+ManOfTheMatchPoints);
-//
-//
-//		Competition::UpdateFootballerPrice(currentFootballer);
-//
-//	}
-//}
-//
-//void Competition::UpdateFootballerPoints(queue<Game> UserGames, list<Game> CurrentGame) //for both squads of the match
-//{
-//	removeCurrentGame(UserGames, CurrentGame);
-//
-//	Game game = UserGames.front();
-//	auto AwayFootballPlayers = game.getAwayTeam().getFootballPlayer();
-//	auto HomeFootballPlayers = game.getHomeTeam().getFootballPlayer();
-//
-//
-//	while (!game.getHighlightsOfTheMatch().empty()) {
-//
-//		Competition::searchTeamInMatch(AwayFootballPlayers, game);
-//		Competition::searchTeamInMatch(HomeFootballPlayers, game);
-//
-//
-//		game.getHighlightsOfTheMatch().pop();
-//	}
-//
-//
-//
-//
-//}
-//
+#include <iostream>
+#include <queue>
+#include<list>
+#include "Competition.h"
+#include "Teams.h"
+#include"Game/Game.h"
+#include <unordered_map>
+#include "User.h"
+#include <regex>
+
+
+using namespace std;
+
+
+
+
+bool Competition::IsManOfTheMatch(string currentMOTM, string playerName)
+{
+	return (currentMOTM == playerName);
+}
+
+
+bool Competition::checkPosition(string footballerPosition) {
+
+	if (footballerPosition == "defender" || footballerPosition == "goalkeeper") {
+		return true;
+	}
+
+	else
+		return false;
+
+}
+
+char Competition::priceCalculation(int points) {
+
+
+
+	if (points < 3) {
+		return '0';
+	}
+	else if (points < 6) {
+		return '1';
+	}
+	else if (points < 9) {
+		return '2';
+	}
+	else if (points < 12) {
+		return '3';
+	}
+	else if (points < 15) {
+		return '4';
+	}
+	else {
+		return '5';
+	}
+
+}
+
+void Competition::removeCurrentGame(queue<Game> UserGames, list<Game>& allGames)
+{
+	while (!UserGames.empty())
+	{
+		auto gameit = allGames.begin();
+		while (gameit != allGames.end())
+		{
+			if ((*gameit).getGameId() == UserGames.front().getGameId()) {
+				break;
+			}
+		}
+		allGames.erase(gameit);
+
+
+
+
+		UserGames.pop();
+	}
+}
+
+void Competition::ReducePoints(string footballerName, User& currentUser, string violence, string status, Teams& team)
+{
+	if (status == "User") {
+		if (violence == "RedCard")
+		{
+			currentUser.GetMainSquad().at(footballerName).AddTotalpoints(-2);
+			currentUser.AddPoints(-2);
+			currentUser.addBalance(-20);
+		}
+		if (violence == "YellowCard") {
+			currentUser.GetMainSquad().at(footballerName).AddTotalpoints(-1);
+			currentUser.AddPoints(-1);
+			currentUser.addBalance(-5);
+		}
+	}
+	else if (status == "Footballer") {
+		if (violence == "RedCard")
+		{
+			team.getFootballPlayer().at(footballerName).AddTotalpoints(-2);
+		}
+		if (violence == "YellowCard") {
+			team.getFootballPlayer().at(footballerName).AddTotalpoints(-1);
+		}
+	}
+
+
+
+}
+
+void Competition::updatePoints(string footballerName, User& currentUser, string contributes, string status, Teams& team, string footballerPosition)
+{
+	//"goals 5 & assists 6"
+	regex pattern(R"(&)");
+	int numberofGoals;
+	if (regex_match(contributes, pattern)) { // if the player scord and did assist
+
+		addGoalsAssistPoints(contributes, currentUser, footballerName, status, team);
+	}
+
+	else if (!regex_match(contributes, pattern))
+	{
+		regex Goalpattern(R"(Goal)");
+		regex Assistpattern(R"(Assist)");
+		if (regex_match(contributes, Goalpattern)) {
+
+			addPoints(contributes, currentUser, footballerName, Competition::goalPoints, status, team);
+		}
+		else if (regex_match(contributes, Assistpattern)) {
+
+			addPoints(contributes, currentUser, footballerName, Competition::assistPoints, status, team);
+		}
+		else {
+			if (checkPosition(footballerPosition)) {
+				currentUser.GetMainSquad().at(footballerName).AddTotalpoints(cleanSheetPoints);
+				currentUser.AddPoints(cleanSheetPoints);
+				currentUser.addBalance(10);
+			}
+		}
+	}
+	else
+	{
+		currentUser.GetMainSquad().at(footballerName).AddTotalpoints(-10);
+		currentUser.AddPoints(-10);
+	}
+
+}
+
+
+void Competition::addPoints(string contributes, User currentUser, string footballerName, int numPerpoints, string status, Teams& team)
+{
+	int numberOfcontributes;
+	regex pattern(R"(\d+)");
+
+	smatch match;
+
+	if (regex_search(contributes, match, pattern)) {
+		numberOfcontributes = stoi(match[0]);
+
+	}
+	if (status == "footballer") {
+		team.getFootballPlayer().at(footballerName).AddTotalpoints(numPerpoints * numberOfcontributes);
+	}
+	else {
+		currentUser.GetMainSquad().at(footballerName).AddTotalpoints(numPerpoints * numberOfcontributes);
+		currentUser.AddPoints(numPerpoints * numberOfcontributes);
+		currentUser.addBalance(numPerpoints * numberOfcontributes);
+	}
+}
+
+
+
+void Competition::addGoalsAssistPoints(string contributes, User currentUser, string footballerName, string status, Teams& team)
+{
+	regex pattern(R"(\d+)");
+
+	smatch matches;
+	int goalsnum=0, assistsnum=0;
+
+	int foundNumbers = 0;
+
+	while (regex_search(contributes, matches, pattern)) {
+		if (foundNumbers == 0) {
+			goalsnum = stoi(matches[0]);
+		}
+		else {
+			assistsnum = stoi(matches[0]);
+		}
+		contributes = matches.suffix().str();
+		foundNumbers++;
+	}
+	int totalPoints = (goalsnum * Competition::goalPoints) + (assistsnum * Competition::assistPoints);
+	if (status == "footballer") {
+		team.getFootballPlayer().at(footballerName).AddTotalpoints(totalPoints);
+	}
+	else {
+		currentUser.GetMainSquad().at(footballerName).AddTotalpoints(totalPoints);
+		currentUser.AddPoints(totalPoints);
+		currentUser.addBalance(totalPoints * 3);
+	}
+}
+
+void Competition::findPlayers(queue<Game>& UserGames, User& currentUser, string status, Teams& team)
+{
+
+	Game currentGame = UserGames.front();
+	while (!currentGame.getHighlightsOfTheMatch().empty())
+	{
+
+		string currentPlayerinMatch = currentGame.getHighlightsOfTheMatch().top().getName();
+		for (auto& kv : currentUser.GetMainSquad()) {
+			string footballerName = kv.first;
+			if (footballerName == currentPlayerinMatch)
+			{
+				updatePoints(currentPlayerinMatch, currentUser, currentGame.getHighlightsOfTheMatch().top().getContributions(), "User", team);
+				ReducePoints(currentPlayerinMatch, currentUser, currentGame.getHighlightsOfTheMatch().top().getViolation(), "User", team);
+
+			}
+		}
+
+		//go to the next player
+		currentGame.getHighlightsOfTheMatch().pop();
+	}
+
+
+	//The end of the match
+	UserGames.pop();
+}
+
+void Competition::showAllGameHighlights(queue<Game>Usergames, list <Game>& allGame)
+{
+	char ans;
+	removeCurrentGame(Usergames, allGame);
+	cout << "Highlights of the week" << endl;
+	for (Game game : allGame) {
+		cout << game.getAwayTeam().getName() << User::spacing(10, ' ') << game.getHomeTeam().getName() << endl;
+		cout << game.getScore() << endl;
+	}
+}
+
+void Competition::UpdateFootballerPrice(Footballer& player) // for all the players
+{
+	char tier = Competition::priceCalculation(player.GetTotalpoints());
+	float currentPlayerPrice = player.GetPrice();
+	float priceChange;
+
+	switch (tier) {
+
+	case '1':
+		priceChange = currentPlayerPrice + 0.5f;
+		break;
+
+	case '2':
+		priceChange = currentPlayerPrice + 1.0f;
+		break;
+
+	case '3':
+		priceChange = currentPlayerPrice + 1.5f;
+		break;
+
+	case '4':
+		priceChange = currentPlayerPrice + 2.0f;
+		break;
+
+	case '5':
+		priceChange = currentPlayerPrice + 2.5f;
+		break;
+
+	default:
+		//if the player doesn't perfom with any of the tiers
+		priceChange = currentPlayerPrice - 0.2f;
+	}
+
+	player.SetPrice(priceChange);
+	player.SetTotalpoints(0);
+
+
+}
+
+void Competition::searchTeamInMatch(unordered_map<string, Footballer> TeamType, Game game) {
+
+
+	Teams team;
+	User currentUser;
+	string status = "footballer";
+	HighlightsOfTheMatch highlights = game.getHighlightsOfTheMatch().top();
+
+
+	string currentMOTM = game.getManOfTheMatch();
+
+	for (auto& kv : TeamType) {
+
+		string playerName = kv.first;
+		Footballer& currentFootballer = kv.second;
+
+		int ManOfTheMatchPoints = 0;
+		string contributions = highlights.getContributions();
+		string violation = highlights.getViolation();
+
+
+
+		if (Competition::IsManOfTheMatch(currentMOTM, currentFootballer.GetName()) == true) {
+			ManOfTheMatchPoints = Competition::MOTM_Bonus;
+		}
+
+		Competition::updatePoints(playerName, currentUser, contributions, status, team,currentFootballer.GetPosition());
+		Competition::ReducePoints(playerName, currentUser, violation, status, team); // for deducing redCards and yellowcards points
+
+		int currentPoints = currentFootballer.GetTotalpoints();
+		currentFootballer.AddTotalpoints(currentPoints+ManOfTheMatchPoints);
+
+
+		Competition::UpdateFootballerPrice(currentFootballer);
+
+	}
+}
+
+void Competition::UpdateFootballerPoints(queue<Game> UserGames, list<Game> CurrentGame) //for both squads of the match for the user
+{
+	removeCurrentGame(UserGames, CurrentGame);
+
+	Game game = UserGames.front();
+	auto AwayFootballPlayers = game.getAwayTeam().getFootballPlayer();
+	auto HomeFootballPlayers = game.getHomeTeam().getFootballPlayer();
+
+
+	while (!game.getHighlightsOfTheMatch().empty()) {
+
+		Competition::searchTeamInMatch(AwayFootballPlayers, game);
+		Competition::searchTeamInMatch(HomeFootballPlayers, game);
+
+
+		game.getHighlightsOfTheMatch().pop();
+	}
+
+
+
+
+}
+
+void Competition::UpdateAllPlayersPoints(list<Game>GameWeeks) {
+
+	list<Game>CurrentGameWeek = GameWeeks; // for now
+	if (CurrentGameWeek.empty()) {
+		return;
+	}
+
+	Game CurrentGame;
+	int numOfMatchesPlayed = 0; 
+	while (numOfMatchesPlayed < 8) {
+
+		CurrentGame = CurrentGameWeek.front();
+
+
+
+
+
+		CurrentGameWeek.pop_front();
+		numOfMatchesPlayed++;
+	}
+
+
+}
+
--- Competition/Game/Game.cpp
+++ Competition/Game/Game.cpp
@@ -1,157 +1,157 @@
-//#include <iostream>
-//#include <sstream>
-//#include <iomanip>
-//#include <random>
-//#include "Game.h"
-//#include"Teams.h"
-////#include "League.h"
-//#include<regex>
-//
-//
-//
-//
-//using namespace std;
-//
-//
-//void Game::displayBorder(int type) {
-//
-//	if (type == 1) {
-//		cout << "~-~-~-~-~-~-~-~-~-~-~~-~-~-~-~-~-~-~-~-~-~~-~-~-~-~-~-~-~-~-~-~~-~-~-~-~-~-~-~-~-~-~\n"
-//			"------------------------------------------------------------------------------------\n"
-//			"~-~-~-~-~-~-~-~-~-~-~~-~-~-~-~-~-~-~-~-~-~~-~-~-~-~-~-~-~-~-~-~~-~-~-~-~-~-~-~-~-~-~\n"
-//			"\n";
-//	}
-//	else if (type == 2) {
-//		cout << string(86, '_') << endl;
-//	}
-//	else if (type == 2) {
-//		cout << string(86, '-') << endl;
-//	}
-//}
-//
-//void Game::displayTeamsAndScore(Game currentGame)
-//{
-//	string homeTeamName = currentGame.getHomeTeam().getName();
-//	string awayTeamName = currentGame.getAwayTeam().getName();
-//
-//	cout << right << setw(20) << "\u2022" << homeTeamName << setw(20) << currentGame.getScore() << setw(25) << "\u2022" << awayTeamName << endl;
-//
-//}
-//
-//void Game::displayStatisitcs(Game currentGame)
-//{
-//	string matchStats = currentGame.getStatistics();
-//
-//	regex pattern("(\\w+) (\\d+-\\d+)");
-//
-//	// Iterate over matches
-//	auto words_begin = sregex_iterator(matchStats.begin(), matchStats.end(), pattern);
-//	auto words_end = sregex_iterator();
-//
-//	for (sregex_iterator i = words_begin; i != words_end; ++i) {
-//		smatch match = *i;
-//		string word = "~~" + match[1].str() + "~~"; //statistics names
-//		string score = match[2]; //statistics scores
-//
-//		// Calculate the padding 
-//		int padding_name = (89 - word.length()) / 2;
-//		int padding_score = (89 - score.length()) / 2;
-//
-//
-//		cout << setw(padding_name + word.length()) << word << endl;
-//		cout << setw(padding_score + score.length()) << score << endl;
-//
-//		cout << "\n";
-//
-//	}
-//}
-//
-//Game::Game()
-//{
-//	    awayTeam = Teams();
-//        homeTeam = Teams();
-//        winningTeam = "";
-//        score = "";
-//        manOfTheMatch = "";
-//        statistics = "";
-//        date = "";
-//        GameId = 0;
-//}
-////, Teams home, Teams away
-//
-//Game::Game(int id, string winTeam, string score,
-//	string stats, stack<HighlightsOfTheMatch> highlights, string motm, string date)
-//{
-//	this->GameId = id;
-//	//this->awayTeam = away;
-//	//this->homeTeam = home;
-//	this->winningTeam = winTeam;
-//	this->score = score;
-//	this->manOfTheMatch = motm;
-//	this->statistics = stats;
-//	this->highlightsOfTheMatch = highlights;
-//	this->date = date;
-//}
-//
-//void Game::displayPlayerHighlights(Game game) {
-//
-//	Game CurrentGame = game; // for poping the stack without changing the actual one
-//	HighlightsOfTheMatch currentHighlight = CurrentGame.getHighlightsOfTheMatch().top();
-//	string playerName = CurrentGame.getHighlightsOfTheMatch().top().getName();
-//	string Contributes = CurrentGame.getHighlightsOfTheMatch().top().getContributions();
-//	string violation = CurrentGame.getHighlightsOfTheMatch().top().getViolation();
-//
-//	size_t found_goals = Contributes.find("goals");
-//	size_t foundYellowCards = violation.find("yellowCard");
-//	size_t foundRedCards = violation.find("redCard");
-//	random_device rd;
-//	mt19937 gen(rd()); // ----> engine used for generating random numbers
-//
-//	int CurrentMin = 1;
-//	int max = 90;
-//
-//	uniform_int_distribution<> dis;
-//
-//	int goalsNumber = 0;
-//	if (found_goals != string::npos) {
-//		// Extracting the number of goals to be used in output
-//		goalsNumber = stoi(Contributes.substr(found_goals + 6));
-//
-//		for (int i = 0; i < goalsNumber; i++) {
-//
-//			dis = uniform_int_distribution<>(CurrentMin, max);
-//			int GoalMinute = dis(gen);
-//			cout << right << setw(45) << playerName << " " << GoalMinute << "'" << "\U000026BD" << endl;
-//			cout << "\n";
-//			CurrentMin = GoalMinute + 1;
-//
-//		}
-//
-//	}
-//
-//
-//	CurrentGame.getHighlightsOfTheMatch().pop();
-//
-//}
-//
-//
-//void Game::displayGameOverview(queue <Game> currentGame) {
-//
-//	Game game;
-//
-//
-//	system("cls");
-//	Game::displayBorder(1);
-//	cout << left << setw(50) << game.getDate() << right << setw(33) << "Full-time" << endl;
-//	cout << "\n";
-//	displayTeamsAndScore(game);
-//	Game::displayBorder(2);
-//	cout << right << setw(30) << "\u2605" << " Man of the Match : " << game.getManOfTheMatch() << endl;
-//	Game::displayBorder(2);
-//	cout << "\n\n";
-//	Game::displayPlayerHighlights(game);
-//	Game::displayBorder(3);
-//
-//
-//}
-//
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <random>
+#include "Game.h"
+#include"Teams.h"
+//#include "League.h"
+#include<regex>
+
+
+
+
+using namespace std;
+
+
+void Game::displayBorder(int type) {
+
+	if (type == 1) {
+		cout << "~-~-~-~-~-~-~-~-~-~-~~-~-~-~-~-~-~-~-~-~-~~-~-~-~-~-~-~-~-~-~-~~-~-~-~-~-~-~-~-~-~-~\n"
+			"------------------------------------------------------------------------------------\n"
+			"~-~-~-~-~-~-~-~-~-~-~~-~-~-~-~-~-~-~-~-~-~~-~-~-~-~-~-~-~-~-~-~~-~-~-~-~-~-~-~-~-~-~\n"
+			"\n";
+	}
+	else if (type == 2) {
+		cout << string(86, '_') << endl;
+	}
+	else if (type == 2) {
+		cout << string(86, '-') << endl;
+	}
+}
+
+void Game::displayTeamsAndScore(Game currentGame)
+{
+	string homeTeamName = currentGame.getHomeTeam().getName();
+	string awayTeamName = currentGame.getAwayTeam().getName();
+
+	cout << right << setw(20) << "\u2022" << homeTeamName << setw(20) << currentGame.getScore() << setw(25) << "\u2022" << awayTeamName << endl;
+
+}
+
+void Game::displayStatisitcs(Game currentGame)
+{
+	string matchStats = currentGame.getStatistics();
+
+	regex pattern("(\\w+) (\\d+-\\d+)");
+
+	// Iterate over matches
+	auto words_begin = sregex_iterator(matchStats.begin(), matchStats.end(), pattern);
+	auto words_end = sregex_iterator();
+
+	for (sregex_iterator i = words_begin; i != words_end; ++i) {
+		smatch match = *i;
+		string word = "~~" + match[1].str() + "~~"; //statistics names
+		string score = match[2]; //statistics scores
+
+		// Calculate the padding 
+		int padding_name = (89 - word.length()) / 2;
+		int padding_score = (89 - score.length()) / 2;
+
+
+		cout << setw(padding_name + word.length()) << word << endl;
+		cout << setw(padding_score + score.length()) << score << endl;
+
+		cout << "\n";
+
+	}
+}
+
+Game::Game()
+{
+	    awayTeam = Teams();
+        homeTeam = Teams();
+        winningTeam = "";
+        score = "";
+        manOfTheMatch = "";
+        statistics = "";
+        date = "";
+        GameId = 0;
+}
+//, Teams home, Teams away
+
+Game::Game(int id, string winTeam, string score,
+	string stats, stack<HighlightsOfTheMatch> highlights, string motm, string date)
+{
+	this->GameId = id;
+	//this->awayTeam = away;
+	//this->homeTeam = home;
+	this->winningTeam = winTeam;
+	this->score = score;
+	this->manOfTheMatch = motm;
+	this->statistics = stats;
+	this->highlightsOfTheMatch = highlights;
+	this->date = date;
+}
+
+void Game::displayPlayerHighlights(Game game) {
+
+	Game CurrentGame = game; // for poping the stack without changing the actual one
+	HighlightsOfTheMatch currentHighlight = CurrentGame.getHighlightsOfTheMatch().top();
+	string playerName = CurrentGame.getHighlightsOfTheMatch().top().getName();
+	string Contributes = CurrentGame.getHighlightsOfTheMatch().top().getContributions();
+	string violation = CurrentGame.getHighlightsOfTheMatch().top().getViolation();
+
+	size_t found_goals = Contributes.find("goals");
+	size_t foundYellowCards = violation.find("yellowCard");
+	size_t foundRedCards = violation.find("redCard");
+	random_device rd;
+	mt19937 gen(rd()); // ----> engine used for generating random numbers
+
+	int CurrentMin = 1;
+	int max = 90;
+
+	uniform_int_distribution<> dis;
+
+	int goalsNumber = 0;
+	if (found_goals != string::npos) {
+		// Extracting the number of goals to be used in output
+		goalsNumber = stoi(Contributes.substr(found_goals + 6));
+
+		for (int i = 0; i < goalsNumber; i++) {
+
+			dis = uniform_int_distribution<>(CurrentMin, max);
+			int GoalMinute = dis(gen);
+			cout << right << setw(45) << playerName << " " << GoalMinute << "'" << "\U000026BD" << endl;
+			cout << "\n";
+			CurrentMin = GoalMinute + 1;
+
+		}
+
+	}
+
+
+	CurrentGame.getHighlightsOfTheMatch().pop();
+
+}
+
+
+void Game::displayGameOverview(queue <Game> currentGame) {
+
+	Game game;
+
+
+	system("cls");
+	Game::displayBorder(1);
+	cout << left << setw(50) << game.getDate() << right << setw(33) << "Full-time" << endl;
+	cout << "\n";
+	displayTeamsAndScore(game);
+	Game::displayBorder(2);
+	cout << right << setw(30) << "\u2605" << " Man of the Match : " << game.getManOfTheMatch() << endl;
+	Game::displayBorder(2);
+	cout << "\n\n";
+	Game::displayPlayerHighlights(game);
+	Game::displayBorder(3);
+
+
+}
+
