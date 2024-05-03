7132efaf3760027008b16a8ef8d056ce93946fa7
Abdelrahman339 <abdelrahmanamr.eltawab@gmail.com>
2024-05-03 18:45:00 +0300

    teams and footballer done

--- Data/Teams/FootballerGetterSetter.cpp
+++ Data/Teams/FootballerGetterSetter.cpp
@@ -1,137 +1,137 @@
-//#include <iostream>	
-//#include <vector>
-//
-//#include "Footballer.h"
-//
-//string Footballer::GetName() {
-//	return this->name;
-//}
-//Footballer::Footballer() {
-//	this->name = "";
-//	this->age = 0;
-//	this->team = "";
-//	this->position = "";
-//	this->price = 80.0F;
-//	this->rating = 2.0F;
-//	this->captain = false;
-//	this->matchWinner = false;
-//	this->totalGoals = 0;
-//	this->totalAssists = 0;
-//	this->totalRedCard = 0;
-//	this->totalYellowCard = 0;
-//	this->totalCleansheets = 0;
-//	this->totalPoints = 0;
-//}
-//Footballer::Footballer(string name, int age, string team, string position, float price, float rating,
-//	int totalGoals, int totalAssists, int totalRedCard, int totalYellowCard,
-//	int totalCleansheets, int totalPoints
-//) {
-//	this->name = name;
-//	this->age = age;
-//	this->team = team;
-//	this->position = position;
-//	this->price = price;
-//	this->rating = rating;
-//	//this->captain = false;
-//	//this->matchWinner = false;
-//	this->totalGoals = totalGoals;
-//	this->totalAssists = totalAssists;
-//	this->totalRedCard = totalRedCard;
-//	this->totalYellowCard = totalYellowCard;
-//	this->totalCleansheets = totalCleansheets;
-//	this->totalPoints = totalPoints;
-//}
-//
-//
-//void Footballer::SetName(string name) {
-//	this->name = name;
-//}
-//
-//int Footballer::GetAge() {
-//	return age;
-//}
-//
-//void Footballer::SetAge(int age) {
-//	this->age = age;
-//}
-//
-//void Footballer::SetTeam(string team) {
-//	this->team = team;
-//}
-//
-//string Footballer::GetTeam() {
-//	return team;
-//}
-//string Footballer::GetPosition() {
-//	return position;
-//}
-//
-//void Footballer::SetPosition(string position) {
-//	this->position = position;
-//}
-//
-//float Footballer::GetPrice() {
-//	return price;
-//}
-//
-//void Footballer::SetPrice(float price) {
-//	this->price = price;
-//}
-//
-//bool Footballer::GetCaptain() {
-//	return captain;
-//}
-//
-//void Footballer::SetCaptain(bool captain) {
-//	this->captain = captain;
-//}
-//
-//
-//void Footballer::SetRating(float rating) {
-//	this->rating = rating;
-//};
-//void Footballer::SetTotalGoals(int goals) {
-//	this->totalGoals = goals;
-//};
-//void Footballer::SetTotalAssists(int assists) {
-//	this->totalAssists = assists;
-//};
-//void Footballer::SetTotalYellowcard(int yellowCard) {
-//	this->totalYellowCard = yellowCard;
-//};
-//void Footballer::SetTotalRedCard(int redCard) {
-//	this->totalRedCard = redCard;
-//};
-//int Footballer::GetTotalGoals() {
-//	return totalGoals;
-//}
-//int Footballer::GetTotalAssists() {
-//	return totalAssists;
-//}
-//int Footballer::GetTotalRedCard() {
-//	return totalRedCard;
-//}
-//int Footballer::GetTotalYellowCard() {
-//	return totalYellowCard;
-//}
-//int Footballer::GetTotalCleansheets() {
-//	return totalCleansheets;
-//}
-//
-//int Footballer::GetTotalpoints() {
-//	return totalPoints;
-//}
-//
-//void Footballer::SetTotalCleansheets(int cleanSheets) {
-//	this->totalCleansheets = cleanSheets;
-//};
-//void Footballer::SetTotalpoints(int points) {
-//	this->totalPoints = points;
-//}
-//void Footballer::AddTotalpoints(int points)
-//{
-//	this->totalPoints += points;
-//}
-//;
-//
-//
+#include <iostream>	
+#include <vector>
+
+#include "Footballer.h"
+
+string Footballer::GetName() {
+	return this->name;
+}
+Footballer::Footballer() {
+	this->name = "";
+	this->age = 0;
+	this->team = "";
+	this->position = "";
+	this->price = 80.0F;
+	this->rating = 2.0F;
+	this->captain = false;
+	this->matchWinner = false;
+	this->totalGoals = 0;
+	this->totalAssists = 0;
+	this->totalRedCard = 0;
+	this->totalYellowCard = 0;
+	this->totalCleansheets = 0;
+	this->totalPoints = 0;
+}
+Footballer::Footballer(string name, int age, string team, string position, float price, float rating,
+	int totalGoals, int totalAssists, int totalRedCard, int totalYellowCard,
+	int totalCleansheets, int totalPoints
+) {
+	this->name = name;
+	this->age = age;
+	this->team = team;
+	this->position = position;
+	this->price = price;
+	this->rating = rating;
+	//this->captain = false;
+	//this->matchWinner = false;
+	this->totalGoals = totalGoals;
+	this->totalAssists = totalAssists;
+	this->totalRedCard = totalRedCard;
+	this->totalYellowCard = totalYellowCard;
+	this->totalCleansheets = totalCleansheets;
+	this->totalPoints = totalPoints;
+}
+
+
+void Footballer::SetName(string name) {
+	this->name = name;
+}
+
+int Footballer::GetAge() {
+	return age;
+}
+
+void Footballer::SetAge(int age) {
+	this->age = age;
+}
+
+void Footballer::SetTeam(string team) {
+	this->team = team;
+}
+
+string Footballer::GetTeam() {
+	return team;
+}
+string Footballer::GetPosition() {
+	return position;
+}
+
+void Footballer::SetPosition(string position) {
+	this->position = position;
+}
+
+float Footballer::GetPrice() {
+	return price;
+}
+
+void Footballer::SetPrice(float price) {
+	this->price = price;
+}
+
+bool Footballer::GetCaptain() {
+	return captain;
+}
+
+void Footballer::SetCaptain(bool captain) {
+	this->captain = captain;
+}
+
+
+void Footballer::SetRating(float rating) {
+	this->rating = rating;
+};
+void Footballer::SetTotalGoals(int goals) {
+	this->totalGoals = goals;
+};
+void Footballer::SetTotalAssists(int assists) {
+	this->totalAssists = assists;
+};
+void Footballer::SetTotalYellowcard(int yellowCard) {
+	this->totalYellowCard = yellowCard;
+};
+void Footballer::SetTotalRedCard(int redCard) {
+	this->totalRedCard = redCard;
+};
+int Footballer::GetTotalGoals() {
+	return totalGoals;
+}
+int Footballer::GetTotalAssists() {
+	return totalAssists;
+}
+int Footballer::GetTotalRedCard() {
+	return totalRedCard;
+}
+int Footballer::GetTotalYellowCard() {
+	return totalYellowCard;
+}
+int Footballer::GetTotalCleansheets() {
+	return totalCleansheets;
+}
+
+int Footballer::GetTotalpoints() {
+	return totalPoints;
+}
+
+void Footballer::SetTotalCleansheets(int cleanSheets) {
+	this->totalCleansheets = cleanSheets;
+};
+void Footballer::SetTotalpoints(int points) {
+	this->totalPoints = points;
+}
+void Footballer::AddTotalpoints(int points)
+{
+	this->totalPoints += points;
+}
+;
+
+
--- Data/Teams/Teams.cpp
+++ Data/Teams/Teams.cpp
@@ -1,83 +1,83 @@
-//#include "Teams.h"
-//
-//Teams::Teams() {
-//	this->name = "";
-//	this->points = 0;
-//	this->wins = 0;
-//	this->lose = 0;
-//	this->draw = 0;
-//	this->players = {};
+#include "Teams.h"
+
+Teams::Teams() {
+	this->name = "";
+	this->points = 0;
+	this->wins = 0;
+	this->lose = 0;
+	this->draw = 0;
+	this->players = {};
+}
+
+Teams::Teams(string name, int points, int wins, int lose, int draw, unordered_map<string, Footballer> footballers) {
+	this->name = name;
+	this->points = points;
+	this->wins = wins;
+	this->lose = lose;
+	this->draw = draw;
+	this->players = footballers;
+}
+
+unordered_map<string, Footballer> Teams::getFootballPlayer()
+{
+	return this->players;
+}
+//Teams Teams::getTeamByName(map<string, Teams> teams, string name) {
+//	return teams.at(name);
 //}
+
+string Teams::getName()
+{
+	return name;
+}
+
+int Teams::getwins()
+{
+	return wins;
+}
+
+int Teams::getlose()
+{
+	return lose;
+}
+
+int Teams::getdraw()
+{
+	return draw;
+}
+
+int Teams::getPoints()
+{
+	return points;
+}
+
+void Teams::SetPoints(int num)
+{
+	this->points = num;
+}
+
+void Teams::addPoints(int num)
+{
+	points += num;
+}
+//Teams Teams::getTeamByName(vector<League> allleagues, string teamName) {
+//	Teams team;
 //
-//Teams::Teams(string name, int points, int wins, int lose, int draw, unordered_map<string, Footballer> footballers) {
-//	this->name = name;
-//	this->points = points;
-//	this->wins = wins;
-//	this->lose = lose;
-//	this->draw = draw;
-//	this->players = footballers;
+//	for (size_t i = 0; i < allleagues.size(); i++) {
+//		try {
+//			team = allleagues[i].GetTeams().at(teamName);
+//			break;
+//		}
+//		catch (const std::exception&) {
+//			cout << "Couldn't find the specified team";
+//			return {};
+//		}
+//	}
+//	return team;
 //}
-//
-//unordered_map<string, Footballer> Teams::getFootballPlayer()
+//Footballer* Teams::getFootballerByTeamName(Teams team, string footballerName)
 //{
-//	return this->players;
+//	Footballer* footballerToBeUpdated = nullptr;
+//	*footballerToBeUpdated = team.getFootballPlayer().at(footballerName);
+//	return footballerToBeUpdated;
 //}
-////Teams Teams::getTeamByName(map<string, Teams> teams, string name) {
-////	return teams.at(name);
-////}
-//
-//string Teams::getName()
-//{
-//	return name;
-//}
-//
-//int Teams::getwins()
-//{
-//	return wins;
-//}
-//
-//int Teams::getlose()
-//{
-//	return lose;
-//}
-//
-//int Teams::getdraw()
-//{
-//	return draw;
-//}
-//
-//int Teams::getPoints()
-//{
-//	return points;
-//}
-//
-//void Teams::SetPoints(int num)
-//{
-//	this->points = num;
-//}
-//
-//void Teams::addPoints(int num)
-//{
-//	points += num;
-//}
-////Teams Teams::getTeamByName(vector<League> allleagues, string teamName) {
-////	Teams team;
-////
-////	for (size_t i = 0; i < allleagues.size(); i++) {
-////		try {
-////			team = allleagues[i].GetTeams().at(teamName);
-////			break;
-////		}
-////		catch (const std::exception&) {
-////			cout << "Couldn't find the specified team";
-////			return {};
-////		}
-////	}
-////	return team;
-////}
-////Footballer* Teams::getFootballerByTeamName(Teams team, string footballerName)
-////{
-////	Footballer* footballerToBeUpdated = nullptr;
-////	*footballerToBeUpdated = team.getFootballPlayer().at(footballerName);
-////	return footballerToBeUpdated;
-////}
